86ac896082ad44fa1972207be8ecde4b9711657f
Riyazo4 <mohammedriyazriyaz04@gmail.com>
2023-08-11 15:25:34 +0530

    add domain

--- client/src/axios/axios.jsx
+++ client/src/axios/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const connection = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://circle-up.online/api",
 });
 
 connection.interceptors.request.use(
