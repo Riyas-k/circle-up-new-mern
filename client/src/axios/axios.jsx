e20e46cacba44308a48c164e4f424b2f5ad9ab21
Riyazo4 <mohammedriyazriyaz04@gmail.com>
2023-08-11 21:41:34 +0530

    base url

--- client/src/axios/axios.jsx
+++ client/src/axios/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const connection = axios.create({
-  baseURL: "http://circle-up.online/api",
+  baseURL: "http://ww2.circle-up.online/api",
 });
 
 connection.interceptors.request.use(
